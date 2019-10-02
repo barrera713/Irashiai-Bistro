b0371a27a29a11a8481639e5e1933cf9d4df0fd4
Carlos Barrera <barrera9696@gmail.com>
2019-10-01 20:58:06 -0500

    restyled nav

--- website/src/Components/Home.js
+++ website/src/Components/Home.js
@@ -8,19 +8,33 @@ class Home extends React.Component {
     render() {
         
         return(
+            <div>
             <section className="section-a">
-                <h1>Section A</h1>
-                <ul>
-                    <h3>Hours</h3>
-                    <li>Monday: Closed</li>
-                    <li>Tuesday: 11:00 AM - 9:30 PM</li>
-                    <li>Wednesday: 11:00 AM - 9:30 PM</li>
-                    <li>Thursday: 11:00 AM - 9:30 PM</li>
-                    <li>Friday: 11:00 AM - 9:30 PM</li>
-                    <li>Saturday: 11:00 AM - 9:30 PM</li>
-                    <li>Sunday: Closed</li>
-                </ul>
+                <div className="img-container">
+                    {/* <img src='/roll-3.jpg' alt=""/>
+                    <img src='roll-2.jpg' alt=""/> */}
+                </div>
             </section>
+            <section className="section-b">
+                <h1>Section B</h1>
+                <div>
+                    <ul>
+                        <h3>Hours</h3>
+                        <li>Monday: Closed</li>
+                        <li>Tuesday: 11:00 AM - 9:30 PM</li>
+                        <li>Wednesday: 11:00 AM - 9:30 PM</li>
+                        <li>Thursday: 11:00 AM - 9:30 PM</li>
+                        <li>Friday: 11:00 AM - 9:30 PM</li>
+                        <li>Saturday: 11:00 AM - 9:30 PM</li>
+                        <li>Sunday: Closed</li>
+                    </ul>
+                </div>
+            </section>
+            <section className="section-c">
+                <h4>Section C</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, nobis temporibus! Alias amet perspiciatis libero id eligendi, omnis hic vel voluptas iure a exercitationem suscipit fugit ea eaque? Saepe, facere.</p>
+            </section>
+            </div>
         )
     }
 
--- website/src/Components/NavBar.js
+++ website/src/Components/NavBar.js
@@ -6,12 +6,25 @@ class NavBar extends React.Component {
     render() {
         
         return(
-            <nav>
-                <img src='/logo.jpg' className="logo-img" alt="" />
+            <div className="nav-container">
+                <div className="logo">
+                    <img src="/logo.jpg" alt=""/>
+                </div>
                 <div>
                     <h3>Irashiai Bistro II</h3>
+                <div className="info-btns">
+                    <button>Contact Us</button>
+                    <button>Get Directions</button>
                 </div>
-            </nav>
+                </div>
+                <div className="social-media-container">
+                    <ul className="social-media-links">
+                        <li>IG</li>
+                        <li>FB</li>
+                        <li>YP</li>
+                    </ul>
+                </div>
+            </div>
         )
     }
 
