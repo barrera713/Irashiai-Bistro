c57d22cc58afad9ea55b0bb08faa762f4db94968
Carlos Barrera <barrera9696@gmail.com>
2019-11-13 23:36:33 -0600

    started styling about section

--- website/src/Components/About.js
+++ website/src/Components/About.js
@@ -3,21 +3,17 @@ import React from 'react';
 
 export default function About() {
 
-    return(<div>
-        <div className="about-section">
-            <h3>About</h3>
-            <ul>
-                <li>
-                    <img src='/mimi-kansan.jpg' alt=''/>
-                </li>
-                <li>
-                <p>Altering your taste for the customer make sense in a business setting. You adapt to what your customers are used to and expect. But when I put that soup to my lips, I was in for a surprise.
-The broth was a perfect balance of flavor and not too oily. The pork was cut into manageable pieces and seasoned to a nice finish. The noodles were not too hard nor soft, and had the right taste. I felt like I was back in Japan.
-After finishing most of the contents, Melissa brought me a small dish of Mimi’s renowned “Hell Sauce.” Made from various fresh peppers and some other secret ingredients, it had a faint orange glow. I took the plunge and added a bit of the spice to the broth. Drinking it in, I realized that the sauce deserved the moniker. I had stepped into a benevolent hell.
-The tonkotsu ramen is a popular item at Irashai 2. Many of the local FIT students and Harris employees will drop by for lunch. Complete with the typical pork bone broth, pork belly, scallions, nori, veggies and boiled egg, this is the perfect lunch for someone short for time. It was the first time I ate crab and mushrooms in ramen, and surprisingly it added a great taste to the soup.</p>
-                </li>
-            </ul>
-        </div>
+    return(<div className="about-section">
+        <h1>Our Story</h1>
+        <ul>
+            <li>
+                <img src='/mimi-kansan.jpg' alt=''/>
+            </li>
+            <li className="about-p-container">
+                <p>Mimi and Kansan began their journey 30 years ago in Greenville, South Carolina. Formally, trained in French and Italian cousine, Kansan dived into authentic Japanese cousine when he and his wife decided to go into business. They've now brought authentic Japanese cousine to Palm Bay Fl where they currently reside.
+                </p>
+            </li>
+        </ul>
     </div>)
 
 }
--- website/src/Menu/Dessert.js
+++ website/src/Menu/Dessert.js
@@ -5,24 +5,22 @@ function Dessert() {
 
     return(<div className="menu-items">
         <div className="category-container">
-            <ul className="category-section">
-                <li>
-                    <h3>Cheesecake Tenpura</h3>
-                    <p>6.95</p>
-                    <h3>Banana Tenpura</h3>
-                    <p>4.95</p>
-                    <h3>Oreo Tenpura</h3>
-                    <p>3.25</p>
-                    <h3>Ice Cream Tenpura</h3>
-                    <p>6.95</p>
-                    <h3>Tenpura Combo: Ice Cream, Banana and Oreo Tenpura</h3>
-                    <p>11.95</p>
-                    <h3>Green Tea</h3>
-                    <p>3.95</p>
-                    <h3>Redbean Ice Cream</h3>
-                    <p>3.95</p>
-                </li>
-            </ul>
+            <div className="category-section">
+                <h3>Cheesecake Tenpura</h3>
+                <p>6.95</p>
+                <h3>Banana Tenpura</h3>
+                <p>4.95</p>
+                <h3>Oreo Tenpura</h3>
+                <p>3.25</p>
+                <h3>Ice Cream Tenpura</h3>
+                <p>6.95</p>
+                <h3>Tenpura Combo: Ice Cream, Banana and Oreo Tenpura</h3>
+                <p>11.95</p>
+                <h3>Green Tea</h3>
+                <p>3.95</p>
+                <h3>Redbean Ice Cream</h3>
+                <p>3.95</p>
+            </div>
         </div>
     </div>)
 
