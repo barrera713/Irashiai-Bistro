b59f676b8d9d8cdf81f767e4c93a0bac7ced19c0
Carlos Barrera <barrera9696@gmail.com>
2019-11-14 23:33:47 -0600

    added social links to footer

--- website/src/Components/Footer.js
+++ website/src/Components/Footer.js
@@ -2,18 +2,28 @@ import React from 'react';
 
 function Footer() {
 
-    return(<div>
-        <ul className="footer">
-            <li>
-            <a href="tel:1-321-312-4540">Contact</a>
-            </li>
-            {/* <li>
-            <a href="mailto: irashiaibistro@gmail.com">Email</a>
-            </li> */}
-            <li>
-            <a href="https://goo.gl/maps/JkyZLS3XToPGxVGK9">Directions</a>
-            </li>
-        </ul> 
+    return(<div className="footer-container">
+        <div className="social-links">
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/Irashiai2/" className="fa fa-facebook"> </a>
+                </li>
+                    <a href="https://www.yelp.com/biz/irashiai-2-japanese-bistro-palm-bay" className="fa fa-yelp"> </a>
+                <li>
+                    <a href="mailto: irashiaibistro@gmail.com" className="glyphicon glyphicon-envelope"> </a>
+                </li>
+            </ul>
+        </div>
+        <div className="contact-info">
+            <ul>
+                <li>
+                    <a href="tel:1-321-312-4540">Contact</a>
+                </li>
+                <li>
+                    <a href="https://goo.gl/maps/JkyZLS3XToPGxVGK9">Directions</a>
+                </li>
+            </ul> 
+        </div>
         <div className="footer-end"></div>
     </div>
     
