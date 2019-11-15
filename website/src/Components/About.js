ca54cc286922a8e9ed58e13011241ea52ceacfd6
Carlos Barrera <barrera9696@gmail.com>
2019-11-14 22:13:45 -0600

    about section layout complete

--- website/src/Components/About.js
+++ website/src/Components/About.js
@@ -4,16 +4,14 @@ import React from 'react';
 export default function About() {
 
     return(<div className="about-section">
-        <h1>Our Story</h1>
-        <ul>
-            <li>
-                <img src='/mimi-kansan.jpg' alt=''/>
-            </li>
-            <li className="about-p-container">
-                <p>Mimi and Kansan began their journey 30 years ago in Greenville, South Carolina. Formally, trained in French and Italian cousine, Kansan dived into authentic Japanese cousine when he and his wife decided to go into business. They've now brought authentic Japanese cousine to Palm Bay Fl where they currently reside.
-                </p>
-            </li>
-        </ul>
+        <div className="about-img">
+            <h1>Our Story</h1>
+            <img src='/mimi-kansan.jpg' alt=''/>
+        </div>
+        <div className="about-content">
+            <p>Mimi and Kansan began their journey 30 years ago in Greenville, South Carolina. Formally, trained in French and Italian cousine, Kansan dived into authentic Japanese cousine when he and his wife decided to go into business. They've now brought authentic Japanese cousine to Palm Bay Fl where they currently reside.
+            </p>
+        </div>
     </div>)
 
 }
